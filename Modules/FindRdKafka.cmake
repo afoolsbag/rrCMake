c11f5fd91de15ea3c44b4a1859b5c12a492d3982
zhengrr <afoolsbag@gmail.com>
2019-06-03 12:36:15 +0800

    feat: 加入 FindRdKafka 脚本

---
+++ Modules/FindRdKafka.cmake
@@ -0,0 +1,77 @@
+# ______ _           _______    _ _   __       __ _
+# |  ___(_)         | | ___ \  | | | / /      / _| |
+# | |_   _ _ __   __| | |_/ /__| | |/ /  __ _| |_| | ____ _
+# |  _| | | '_ \ / _` |    // _` |    \ / _` |  _| |/ / _` | zhengrr
+# | |   | | | | | (_| | |\ \ (_| | |\  \ (_| | | |   < (_| | 2019-06-03 – 2019-06-03
+# \_|   |_|_| |_|\__,_\_| \_\__,_\_| \_/\__,_|_| |_|\_\__,_| Unlicense
+
+cmake_minimum_required(VERSION 3.12)
+cmake_policy(VERSION 3.12)
+
+include_guard()
+
+if(NOT COMMAND find_package_handle_standard_args)
+  include(FindPackageHandleStandardArgs)
+endif()
+
+if(NOT COMMAND add_library_ex)
+  include("${CMAKE_CURRENT_LIST_DIR}/rrCMake/AddLibrary.cmake")
+endif()
+
+if(RdKafka_FOUND)
+  return()
+endif()
+
+#-------------------------------------------------------------------------------
+# UNIX-like
+
+if(UNIX)
+  # <prefix>/include
+  find_path(
+    RdKafka_INCLUDE_DIR
+    NAMES "librdkafka/rdkafka.h"
+          "librdkafka/rdkafkacpp.h")
+  mark_as_advanced(RdKafka_INCLUDE_DIR)
+
+  # <prefix>/lib
+  find_library(
+    RdKafka_rdkafka_LIBRARY
+    NAMES "rdkafka")
+  mark_as_advanced(RdKafka_rdkafka_LIBRARY)
+
+  find_library(
+    RdKafka_rdkafka++_LIBRARY
+    NAMES "rdkafka++")
+  mark_as_advanced(RdKafka_rdkafka++_LIBRARY)
+
+  # package
+  find_package_handle_standard_args(
+    RdKafka
+    DEFAULT_MSG
+    RdKafka_rdkafka_LIBRARY
+    RdKafka_INCLUDE_DIR
+    RdKafka_rdkafka++_LIBRARY)
+
+  # targets
+  if(RdKafka_FOUND)
+    if(NOT TARGET RdKafka::rdkafka)
+      add_library_ex(
+        RdKafka::rdkafka    SHARED IMPORTED
+        PROPERTIES          IMPORTED_LOCATION "${RdKafka_rdkafka_LIBRARY}"
+        INCLUDE_DIRECTORIES INTERFACE         "${RdKafka_INCLUDE_DIR}")
+    endif()
+
+    if(NOT TARGET RdKafka::rdkafka++)
+      add_library_ex(
+        RdKafka::rdkafka++  SHARED IMPORTED
+        PROPERTIES          IMPORTED_LOCATION "${RdKafka_rdkafka++_LIBRARY}"
+        INCLUDE_DIRECTORIES INTERFACE         "${RdKafka_INCLUDE_DIR}"
+        LINK_LIBRARIES      INTERFACE         RdKafka::rdkafka++)
+    endif()
+
+  endif()
+
+  # return
+  return()
+
+endif()
