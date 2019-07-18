7296579413f0c0c11de5f7767501dd82407ac884
zhengrr <afoolsbag@gmail.com>
2019-07-18 15:24:41 +0800

    feat(PackageManagersHelper): 使用 macro 替换 function 以减少封装造成的差异

--- Modules/rrCMake/PackageManagersHelper.cmake
+++ Modules/rrCMake/PackageManagersHelper.cmake
@@ -11,7 +11,7 @@ include_guard()
 # .. command:: include_conan_script
 #
 #   检查、下载并包含 `conan` 脚本。
-function(include_conan_script)
+macro(include_conan_script)
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake script")
     file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
@@ -26,13 +26,13 @@ function(include_conan_script)
     message(STATUS "Downloading conan.cmake script - done")
   endif()
   include("${CMAKE_BINARY_DIR}/conan.cmake")
-endfunction()
+endmacro()
 
 #.rst:
 # .. command:: include_hunter_gate_script
 #
 #   检查、下载并包含 `HunterGate` 脚本。
-function(include_hunter_gate_script)
+macro(include_hunter_gate_script)
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/HunterGate.cmake")
     message(STATUS "Downloading HunterGate.cmake script")
     file(DOWNLOAD "https://raw.githubusercontent.com/hunter-packages/gate/master/cmake/HunterGate.cmake"
@@ -53,4 +53,4 @@ function(include_hunter_gate_script)
   HunterGate(
     URL  "https://github.com/ruslo/hunter/archive/v0.23.204.tar.gz"
     SHA1 "32cfed254da901f6f184027d530d8da47e035b85")
-endfunction()
+endmacro()
