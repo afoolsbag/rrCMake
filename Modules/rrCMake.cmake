6cff96f1efd0eec65df720013dd4bffb8c564709
zhengrr <afoolsbag@gmail.com>
2020-05-01 14:59:07 +0800

    style(rrCMake.cmake): 风格变动

--- Modules/rrCMake.cmake
+++ Modules/rrCMake.cmake
@@ -2,23 +2,27 @@
 #           /  __ \|  \/  |     | |
 #  _ __ _ __| /  \/| .  . | __ _| | _____
 # | '__| '__| |    | |\/| |/ _` | |/ / _ \ zhengrr
-# | |  | |  | \__/\| |  | | (_| |   <  __/ 2016-10-08 – 2019-11-18
+# | |  | |  | \__/\| |  | | (_| |   <  __/ 2016-10-08 – 2020-05-01
 # |_|  |_|   \____/\_|  |_/\__,_|_|\_\___| Unlicense
 
-#.rst
+#.rst:
 # rrCMake
 # -------
 #
-# zhengrr's CMake utilities.
+# 为 zhengrr 定制的 CMake 工具集。
+#
 
 cmake_minimum_required(VERSION 3.10)
 cmake_policy(VERSION 3.10)
 
 include_guard()  # 3.10
 
-# .ret
-# .. command:: rrcmake_include_module_files
-function(rrcmake_include_module_files)
+#.rst:
+# .. command:: _rrcmake_include_module_files
+#
+#   遍历查找 rrCMake 目录下所有 *.cmake 模块，并将其包含。
+#
+function(_rrcmake_include_module_files)
   file(GLOB zModuleFiles "${CMAKE_CURRENT_LIST_DIR}/rrCMake/*.cmake")
   foreach(pModuleFile IN LISTS zModuleFiles)
     include("${pModuleFile}")
@@ -26,4 +30,4 @@ function(rrcmake_include_module_files)
 endfunction()
 
 # sctipt
-rrcmake_include_module_files()
+_rrcmake_include_module_files()
