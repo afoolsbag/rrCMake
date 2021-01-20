5503437e384ca395b6913dc26081ac2de5c26afc
afoolsbag <afoolsbag@gmail.com>
2021-01-20 13:50:51 +0800

    docs: 整理文档风格，使更贴近 CMake 官方推荐

--- Modules/rrCMake.cmake
+++ Modules/rrCMake.cmake
@@ -2,26 +2,37 @@
 #           /  __ \|  \/  |     | |
 #  _ __ _ __| /  \/| .  . | __ _| | _____
 # | '__| '__| |    | |\/| |/ _` | |/ / _ \ zhengrr
-# | |  | |  | \__/\| |  | | (_| |   <  __/ 2016-10-08 – 2020-06-08
+# | |  | |  | \__/\| |  | | (_| |   <  __/ 2016-10-08 – 2021-01-20
 # |_|  |_|   \____/\_|  |_/\__,_|_|\_\___| Unlicense
 
-#.rst:
-# rrCMake
-# -------
-#
-# 为 zhengrr 定制的 CMake 工具集。
-#
+#[=======================================================================[.rst:
+rrCMake
+-------
+
+为 zhengrr 定制的 CMake 工具集。
+
+在项目 ``CMakeLists.txt`` 中添加以下代码，以引入该工具集：
+
+.. code-block:: cmake
+
+  list(APPEND CMAKE_MODULE_PATH "path/to/this/Modules")
+  include(rrCMake)
+
+#]=======================================================================]
 
 cmake_minimum_required(VERSION 3.10)
 cmake_policy(VERSION 3.10)
 
 include_guard()  # 3.10
 
-#.rst:
-# .. command:: _rrcmake_include_module_files
-#
-#   遍历查找 rrCMake 目录下所有 *.cmake 模块，并逐个包含。
-#
+#[=======================================================================[.rst:
+.. command:: _rrcmake_include_module_files
+
+  内部命令：包含模块文件。
+
+  遍历查找此文件所在目录的 ``rrCMake`` 子目录下的所有 ``*.cmake`` 模块，并逐个包含。
+
+#]=======================================================================]
 function(_rrcmake_include_module_files)
   file(GLOB zModuleFiles "${CMAKE_CURRENT_LIST_DIR}/rrCMake/*.cmake")
   foreach(pModuleFile IN LISTS zModuleFiles)
@@ -29,5 +40,4 @@ function(_rrcmake_include_module_files)
   endforeach()
 endfunction()
 
-# sctipt
 _rrcmake_include_module_files()
