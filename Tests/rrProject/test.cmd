3f2fa2c56727ea9d434e0ea0173b46e029773a60
afoolsbag <afoolsbag@gmail.com>
2021-04-16 11:26:22 +0800

    chore(Tests): 优化测试脚本

--- Tests/rrProject/test.cmd
+++ Tests/rrProject/test.cmd
@@ -27,6 +27,6 @@ CALL :pause_if_double_click
 EXIT /B 0
 
 :pause_if_double_click
-        ECHO %CMDCMDLINE% | FINDSTR /L %COMSPEC% > NUL ^
+        ECHO %CMDCMDLINE% | FINDSTR /L /B %COMSPEC% > NUL ^
                 && PAUSE
         EXIT /B 0
